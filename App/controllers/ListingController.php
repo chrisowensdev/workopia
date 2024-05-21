62331c1080e002122e89d99fd0ce8faf2473305d
Christopher Owens <cowensdev@gmail.com>
2024-05-21 17:14:10 -0400

    Update route for params

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -43,9 +43,9 @@ class ListingController
      *
      * @return void
      */
-    public function show()
+    public function show($params)
     {
-        $id = $_GET['id'] ?? '';
+        $id = $params['id'] ?? '';
 
         $params = [
             'id' => $id
@@ -53,6 +53,11 @@ class ListingController
 
         $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
 
+        // Check if listing exists
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
         loadView('listings/show', [
             'listing' => $listing
         ]);
