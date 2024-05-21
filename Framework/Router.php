e327acc42678ae8b67a2836cb2a210f103d331c1
Christopher Owens <cowensdev@gmail.com>
2024-05-20 20:55:56 -0400

    Refactor error routes/controller

--- Framework/Router.php
+++ Framework/Router.php
@@ -2,6 +2,8 @@
 
 namespace Framework;
 
+use App\Controllers\ErrorController;
+
 class Router
 {
     protected $routes = [];
@@ -76,19 +78,6 @@ class Router
     }
 
     /**
-     * Load error page
-     * 
-     * @param int $httpCode
-     * @return void
-     */
-    public function error($httpCode = 404)
-    {
-        http_response_code($httpCode);
-        loadView("error/{$httpCode}");
-        exit;
-    }
-
-    /**
      * Route the request
      * 
      * @param string $uri
@@ -110,6 +99,6 @@ class Router
             }
         }
 
-        $this->error();
+        ErrorController::notFound();
     }
 }
